b01ad4e86ac4f49fcfb848f63d85ef180ef03265
hbhankhar <hemant@payzli.com>
2024-08-25 15:07:05 +0530

    added permission in the next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply these headers to all routes
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://assets.calendly.com;
+              style-src 'self' 'unsafe-inline' https://assets.calendly.com;
+              frame-src https://calendly.com;
+              img-src 'self' data:;
+            `.replace(/\s{2,}/g, ' ').trim(),
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
